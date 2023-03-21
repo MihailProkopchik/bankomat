faae2ccdf8280eb71fbec6171deea434f95514cb
unknown <raedar5555@gmail.com>
2023-03-21 22:00:21 +0300

    Добавил сейф с купюрами

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,36 +1,71 @@
 'use strict';
 
 let summa; // 500, 200, 100, 50, 20, 10, 5 (1365)
+
+// Внедрить объект сейфа в данную программу.
+// Перепеисать логику программы для выдачи денег уже из сейфа.
+
 let banknots = [];
 
 let body = document.querySelector('body');
+
+let safe = {
+    cas5: 0,
+    cas10: 100,
+    cas50: 100,
+    cas100: 0,
+    fakeNote: 'fake',
+    pullOut(cassete) {
+        if (cassete) {
+            this[cassete] = this[cassete] - 1;
+            return true;
+        } else {
+            return false;
+        }
+    },
+};
 
 body.addEventListener('click', userPrompt);
 
 function userPrompt() {
     summa = +prompt('Введите сумму денег кратную 5руб');
     while (summa > 0) {
-        if (summa >= 500) {
-            banknots.push(500);
-            summa = summa - 500;
-        } else if (summa >= 200) {
-            banknots.push(200);
-            summa = summa - 200;
-        } else if (summa >= 100) {
-            banknots.push(100);
-            summa = summa - 100;
+        if (summa >= 100) {
+            if (safe.cas100 > 0) {
+                if (safe.pullOut('cas100')) {
+                    banknots.push(100);
+                    summa = summa - 100;
+                }
+            } else {
+                return console.log('Нет купюры 100р');
+            }
         } else if (summa >= 50) {
-            banknots.push(50);
-            summa = summa - 50;
-        } else if (summa >= 20) {
-            banknots.push(20);
-            summa = summa - 20;
+            if (safe.cas50 > 0) {
+                if (safe.pullOut('cas50')) {
+                    banknots.push(50);
+                    summa = summa - 50;
+                }
+            } else {
+                return console.log('Нет купюры 50р');
+            }
         } else if (summa >= 10) {
-            banknots.push(10);
-            summa = summa - 10;
+            if (safe.cas10 > 0) {
+                if (safe.pullOut('cas10')) {
+                    banknots.push(10);
+                    summa = summa - 10;
+                }
+            } else {
+                return console.log('Нет купюры 10р');
+            }
         } else if (summa >= 5) {
-            banknots.push(5);
-            summa = summa - 5;
+            if (safe.cas5 > 0) {
+                if (safe.pullOut('cas5') > 0) {
+                    banknots.push(5);
+                    summa = summa - 5;
+                }
+            } else {
+                return console.log('Нет купюры 5р');
+            }
         }
     }
     document.write('<table>');
